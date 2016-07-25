bb9d16ba2e7cbb2aa09278eb4f3b2db4dd1862c4
KalsangYangtso <Kalsang.Yangtso92@guttmanmail.cuny.edu>
2016-07-25 17:08:55 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+
+
+First name: <input type="text" name="FirstName" value="Mickey"><br>
+Last name: <input type="text" name="LastName" value="Mouse"><br>
+<input type="submit" value="Submit">
+
+
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
